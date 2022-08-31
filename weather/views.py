f0ba722226ed28a3039ceece3d272dd6557ceb85
div1103 <div110393@gmail.com>
2022-08-31 12:34:43 +0300

    index

--- weather/views.py
+++ weather/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+import requests
+import json
 
 def index(request):
-    return render(request, 'weather/index.html')
+    app_id = '791a1e4a216bb844c0bc92e88d5b9609'
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + app_id
+    city = 'Minsk'
+    res = requests.get(url.format(city))
+    # print(type(json.loads(res.text)))
+    main = json.loads(res.text)['main']
+    print(type(main))
+    temp = main['temp']
+    return render(request, 'weather/index.html', {'res':res, 'main': main, 'temp':temp})
 
 def weather(request):
     return render(request, 'weather/weather.html')
